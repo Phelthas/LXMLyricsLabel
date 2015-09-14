653a89a35ab0e5003ca5e98e5012cb3ccf9cedf1
Phelthas <luxiaoming@luxiaomingdeiMac.local>
2015-09-14 14:17:20 +0800

    添加另外一个动画效果

---
+++ LXMLyricsLabel/LXMLyricsLabel/LXMBilayerLabel.h
@@ -0,0 +1,13 @@
+//
+//  LXMBilayerLabel.h
+//  LXMLyricsLabel
+//
+//  Created by luxiaoming on 15/9/14.
+//  Copyright (c) 2015年 luxiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LXMBilayerLabel : UILabel
+
+@end
---
+++ LXMLyricsLabel/LXMLyricsLabel/LXMBilayerLabel.m
@@ -0,0 +1,21 @@
+//
+//  LXMBilayerLabel.m
+//  LXMLyricsLabel
+//
+//  Created by luxiaoming on 15/9/14.
+//  Copyright (c) 2015年 luxiaoming. All rights reserved.
+//
+
+#import "LXMBilayerLabel.h"
+
+@implementation LXMBilayerLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
